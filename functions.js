02ccc800430222e2d9f90077526ed780885bac88
samxsam21 <moe.amayreh@gmail.com>
2024-04-09 23:13:52 +0300

    my work

---
+++ functions.js
@@ -0,0 +1,21 @@
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+  };
+  
+  
